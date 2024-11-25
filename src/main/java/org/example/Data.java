a6970adf7b8f323760f8fe0968e5df6b699e6c67
Tina <156555255+Crivera270@users.noreply.github.com>
2024-11-25 05:37:09 -0500

    trying to make soil temp work

---
+++ src/main/java/org/example/Data.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "data", schema = "temp")
+public class Data {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false)
+    private Integer id;
+
+    @Column(name = "DATE")
+    private String date;
+
+    @Column(name = "POINT", nullable = false)
+    private String point;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getPoint() {
+        return point;
+    }
+
+    public void setPoint(String point) {
+        this.point = point;
+    }
+
+}
